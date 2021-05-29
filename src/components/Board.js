427441a81f207a6a9a9f5ff95f82cdb505f33f14
Mehedi Islam Ripon <mehediislamripon@gmail.com>
2021-05-30 00:11:43 +0600

    ready to deploy

---
+++ src/components/Board.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Square from './Square';
+
+function Board(props) {
+   const renderSquare = (i) => (
+      <Square value={props.squares[i]} onClick={() => props.onClick(i)} />
+   );
+
+   return (
+      <div>
+         <div className="board-row">
+            {renderSquare(1)}
+            {renderSquare(2)}
+            {renderSquare(3)}
+         </div>
+         <div className="board-row">
+            {renderSquare(4)}
+            {renderSquare(5)}
+            {renderSquare(6)}
+         </div>
+         <div className="board-row">
+            {renderSquare(7)}
+            {renderSquare(8)}
+            {renderSquare(9)}
+         </div>
+      </div>
+   );
+}
+
+export default Board;
